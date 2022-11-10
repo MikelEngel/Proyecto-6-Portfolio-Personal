855fa3d0cf5a96ed492df7fd35c6d73ccf524653
MikelEngel <angelhevel@gmail.com>
2022-11-10 14:37:16 -0600

    styles aplicados

--- portfolio-personal/src/index.js
+++ portfolio-personal/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { ReactDOM } from "react";
-import App from './App'
-import './assets/index.css'
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+import  ReactDOM  from "react-dom";
+import App from './App'
+import './index.css'
+
+ReactDOM.render(<App/>, document.querySelector("#root"));
