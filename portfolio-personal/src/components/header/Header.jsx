34d310cde355d9f19bbc8310d17ca0fb20fd1b04
MikelEngel <angelhevel@gmail.com>
2022-11-10 18:52:28 -0600

    Header

--- portfolio-personal/src/components/header/Header.jsx
+++ portfolio-personal/src/components/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hola yo soy</h5>
+        <h1>Miguel Ángel Hernández Velasco</h1>
+        <h5 className='text-light' >Ingeniero en Sistemas</h5>
+        <h5 className='text-light' >Master en Diseño Gráfico y Digital</h5>
+        <h5 className='text-light' >Desarrolador Web Full Stack</h5>
+        <CTA />
+
+        <div className='me'>
+          <img src= {ME} alt='me' />
+        </div>
+
+
+
+      </div>
+    </header>
   )
 }
 
